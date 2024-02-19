3af4e29fcb6db6afb02a34ac0bc5dc1171dd9d40
immutable-biome <biome@immutable.com>
2024-02-19 13:32:22 +1100

    getting things working

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,5 +1,5 @@
-import { AspectRatioImage, DUMMY_RASTER_IMAGE_2_URL } from '@biom3/react';
+import { AspectRatioImage, Button, DUMMY_RASTER_IMAGE_2_URL } from '@biom3/react';
 
 export default function Example() {
-  return <AspectRatioImage imageUrl={DUMMY_RASTER_IMAGE_2_URL} />;
+  return <Button variant="primary/destructive">mooo</Button>;
 }
